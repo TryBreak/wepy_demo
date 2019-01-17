db4733ed6ac7fafcc1751bf90653f34d422a77d2
Mark <meichangliang@outlook.com>
2019-01-18 00:52:27 +0800

    demo

--- src/mixins/utils.js
+++ src/mixins/utils.js
@@ -5,14 +5,40 @@ export default class utils extends wepy.mixin {
     mixin: 'This is mixin data.'
   };
   methods = {
-    $_log() {
+    $log() {
       console.log('mixin method tap');
       console.log(this.mixin);
       this.mixin = 'mixin data was changedxxx';
     }
   };
-  $_roundNum = (begin, end) => {
+  $roundNum = (begin, end) => {
+    const num = Math.round(Math.random() * (end - begin) + begin);
+    return num;
+  };
+  $pureData = data => {
+    // 获取纯净的数组和对象
+    return JSON.parse(JSON.stringify(data));
+  };
+  $setTimeout = null; // 全局的
+  $roundNum = (begin, end) => {
+    // 获取随机整数
     var num = Math.round(Math.random() * (end - begin) + begin);
     return num;
+  };
+  $shuffle = arr => {
+    // 数组洗牌
+    let i = arr.length;
+    while (i) {
+      let j = Math.floor(Math.random() * i--);
+      [arr[j], arr[i]] = [arr[i], arr[j]];
+    }
+  };
+  $filterNull = val => {
+    // 将null转化为空字符串
+    if (val) {
+      return val;
+    } else {
+      return '';
+    }
   };
 }
