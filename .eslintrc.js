15d086955ad184fc4971b237d6712586f2c1bb91
Mark <meichangliang@outlook.com>
2019-01-17 17:14:19 +0800

    es lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  extends: 'standard',
   root: true,
   globals: { wx: true },
   parser: 'babel-eslint',
@@ -8,23 +9,21 @@ module.exports = {
   env: {
     browser: true
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
   // required to lint *.wpy files
-  plugins: [
-    'html'
-  ],
+  plugins: ['html'],
   settings: {
     'html/html-extensions': ['.html', '.wpy']
   },
   // add your custom rules here
-  'rules': {
+  rules: {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'space-before-function-paren': 0
+    'space-before-function-paren': 0,
+    semi: ['error', 'always'],
+    'semi-spacing': ['error', { before: false, after: true }]
   }
-}
+};
